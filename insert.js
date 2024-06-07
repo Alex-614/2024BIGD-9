c7d52a7b2d57daed1c750b9279c9cfd3b245c757
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-06-07 10:11:51 +0200

    Update insert.js

--- insert.js
+++ insert.js
@@ -7,7 +7,7 @@ function insertOrUpdateNews(document) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         const dbo = db.db("BigDNews");
-        const query = { url: document.url };
+        const query = { id: document.id };
         
         dbo.collection("news").findOne(query, function(err, result) {
             if (err) throw err;
@@ -15,31 +15,37 @@ function insertOrUpdateNews(document) {
                 let appendFields = {};
 
                 // Append new data to array fields if they are differend.
-                if (JSON.stringify(result.title) !== JSON.stringify(document.title)) {
+                if (result.title[result.title.length - 1].title !== document.title) {
                     appendFields['title'] = { title: document.title, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.commentsEnabled) !== JSON.stringify(document.commentsEnabled)) {
+                if (result.commentsEnabled[result.commentsEnabled.length - 1].comments !== document.commentsEnabled) {
                     appendFields['commentsEnabled'] = { comments: document.commentsEnabled, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.commentCount) !== JSON.stringify(document.commentCount)) {
+                if (result.commentCount[result.commentCount.length - 1].count !== document.commentCount) {
                     appendFields['commentCount'] = { count: document.commentCount, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.tags) !== JSON.stringify(document.tags)) {
+                if (result.tags[result.tags.length - 1].tags.join(',') !== document.tags.join(',')) {
                     appendFields['tags'] = { tags: document.tags, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.videoLength) !== JSON.stringify(document.videoLength)) {
+                if (result.videoLength[result.videoLength.length - 1].length !== document.videoLength) {
                     appendFields['videoLength'] = { length: document.videoLength, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.views) !== JSON.stringify(document.views)) {
+                if (result.views[result.views.length - 1].views !== document.views) {
                     appendFields['views'] = { views: document.views, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.subscribers) !== JSON.stringify(document.subscribers)) {
+                if (result.subscribers[result.subscribers.length - 1].count !== document.subscribers) {
                     appendFields['subscribers'] = { count: document.subscribers, timestamp: new Date() };
                 }
-                if (JSON.stringify(result.transcript) !== JSON.stringify(document.transcript)) {
+                if (result.transcript[result.transcript.length - 1].transcript !== document.transcript) {
                     appendFields['transcript'] = { transcript: document.transcript, timestamp: new Date() };
                 }
-
+                if (result.likeCount[result.likeCount.length - 1].likeCount !== document.likeCount) {
+                    appendFields['likeCount'] = { likeCount: document.likeCount, timestamp: new Date() };
+                }
+                if (result.categories[result.categories.length - 1].categories.join(',') !== document.categories.join(',')) {
+                    appendFields['categories'] = { categories: document.categories, timestamp: new Date() };
+                }
+                
                 // Put insert together.
                 if (Object.keys(appendFields).length > 0) {
                     let updateQuery = {};
@@ -58,7 +64,7 @@ function insertOrUpdateNews(document) {
                     db.close();
                 }
             } else { //New Video
-                //document.timestamp = new Date();
+                document.timestamp = new Date(); 
                 dbo.collection("news").insertOne(document, function(err, res) {
                     if (err) throw err;
                     console.log("Document inserted");
@@ -69,4 +75,3 @@ function insertOrUpdateNews(document) {
     });
 }
 
-module.exports = { insertOrUpdateNews };
