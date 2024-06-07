981f4cd51230b05dd1f062d9f6f32d97d5943ce7
Paul Kuelpmann <paulkuelpman@gmail.com>
2024-06-07 10:57:18 +0200

    Just to see

---
+++ database/mongo/insertExample.js
@@ -0,0 +1,15 @@
+document = {
+    id: "23214332424",
+    title: "Aaron in the Woods",
+    channelId: "2345434",
+    createdAt: "2024-06-07T12:00:00Z",
+    commentsEnabled: true,
+    commentCount: 10,
+    categories: ["cat1", "cat2"],
+    tags: ["tag1", "tag2"],
+    videoLength: "10:30",
+    views: 1000,
+    likeCount: 2,
+    subscribers: 500,
+    transcript: "Hallo"
+};
