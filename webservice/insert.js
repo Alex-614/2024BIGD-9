7401c087290d9790024d191c96a1f9127887d764
Daezu <niklas.daezu@gmail.com>
2024-06-21 10:12:20 +0200

    Insert always option

--- webservice/insert.js
+++ webservice/insert.js
@@ -20,7 +20,7 @@ async function insertOrUpdateNews(document) {
         
         const result = await coll.findOne(query);
 
-        let appendFields = makePretty(result, document);
+        let appendFields = makePretty(result, document, false);
         console.log(appendFields);
         if (result) {
 
@@ -57,7 +57,7 @@ async function insertOrUpdateNews(document) {
 
 }
 
-function makePretty(result, document) {
+function makePretty(result, document, chnagesOnly) {
 
     let appendFields = {};
     if (result == null) {
@@ -68,36 +68,36 @@ function makePretty(result, document) {
     }
     // Append new data to array fields if they are differend.
     let time = new Date(document.timestamp);
-    
+
     // changing fields:
-    if (result == null || result.title[result.title.length - 1].title !== document.title) {
+    if (result == null || result.title[result.title.length - 1].title !== document.title || !chnagesOnly) {
         appendFields['title'] = [{ title: document.title, timestamp: time }];
     }
-    if (result == null || result.commentsEnabled[result.commentsEnabled.length - 1].comments !== document.commentsEnabled) {
+    if (result == null || result.commentsEnabled[result.commentsEnabled.length - 1].comments !== document.commentsEnabled || !chnagesOnly) {
         appendFields['commentsEnabled'] = [{ comments: document.commentsEnabled, timestamp: time }];
     }
-    if (result == null || result.commentCount[result.commentCount.length - 1].count !== document.commentCount) {
+    if (result == null || result.commentCount[result.commentCount.length - 1].count !== document.commentCount || !chnagesOnly) {
         appendFields['commentCount'] = [{ count: document.commentCount, timestamp: time }];
     }
-    if (result == null || result.tags[result.tags.length - 1].tags.join(',') !== document.tags.join(',')) {
+    if (result == null || result.tags[result.tags.length - 1].tags.join(',') !== document.tags.join(',') || !chnagesOnly) {
         appendFields['tags'] = [{ tags: document.tags, timestamp: time }];
     }
-    if (result == null || result.videoLength[result.videoLength.length - 1].length !== document.videoLength) {
+    if (result == null || result.videoLength[result.videoLength.length - 1].length !== document.videoLength || !chnagesOnly) {
         appendFields['videoLength'] = [{ length: document.videoLength, timestamp: time }];
     }
-    if (result == null || result.views[result.views.length - 1].views !== document.views) {
+    if (result == null || result.views[result.views.length - 1].views !== document.views || !chnagesOnly) {
         appendFields['views'] = [{ views: document.views, timestamp: time }];
     }
-    if (result == null || result.subscribers[result.subscribers.length - 1].count !== document.subscribers) {
+    if (result == null || result.subscribers[result.subscribers.length - 1].count !== document.subscribers || !chnagesOnly) {
         appendFields['subscribers'] = [{ count: document.subscribers, timestamp: time }];
     }
-    if (result == null || result.transcript[result.transcript.length - 1].transcript !== document.transcript) {
+    if (result == null || result.transcript[result.transcript.length - 1].transcript !== document.transcript || !chnagesOnly) {
         appendFields['transcript'] = [{ transcript: document.transcript, timestamp: time }];
     }
-    if (result == null || result.likeCount[result.likeCount.length - 1].likeCount !== document.likeCount) {
+    if (result == null || result.likeCount[result.likeCount.length - 1].likeCount !== document.likeCount || !chnagesOnly) {
         appendFields['likeCount'] = [{ likeCount: document.likeCount, timestamp: time }];
     }
-    if (result == null || result.categories[result.categories.length - 1].categories.join(',') !== document.categories.join(',')) {
+    if (result == null || result.categories[result.categories.length - 1].categories.join(',') !== document.categories.join(',') || !chnagesOnly) {
         appendFields['categories'] = [{ categories: document.categories, timestamp: time }];
     }
 
